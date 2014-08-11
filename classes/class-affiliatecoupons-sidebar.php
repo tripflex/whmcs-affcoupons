d8b439a994eb943d2c062e66dea4a8f5d46c9009
Myles McNamara <myles@smyl.es>
2014-08-10 21:07:19 -0400

    clean up code, update sidebar output fixes #25

--- classes/class-affiliatecoupons-sidebar.php
+++ classes/class-affiliatecoupons-sidebar.php
@@ -12,37 +12,45 @@
  * @link         @@link
  */
 
-if (!defined("WHMCS"))
-	die("This file cannot be accessed directly");
+if ( ! defined( "WHMCS" ) ) die( "This file cannot be accessed directly" );
 
 class AffiliateCoupons_Sidebar extends AffiliateCoupons {
 
-	protected static $instance = null;
+	protected static $instance = NULL;
+
+	function __construct() {
+
+		define( "ADMINAREA", TRUE );
+
+	}
+
+	public function output( $vars ) {
+
+		$modulelink = $vars[ 'modulelink' ];
+		$version    = $vars[ 'version' ];
+
+		$sidebar = '<span class="header">
+						<img src="images/icons/addonmodules.png" class="absmiddle" width="16" height="16" />
+						 Pushover Notifications
+					</span>
+					<ul class="menu">
+				        <li><a target="_blank" href="' . $modulelink . '">GitHub</a></li>
+				        <li><a target="_blank" href="' . $modulelink . '/issues">Report Issues</a></li>
+				        <li><a target="_blank" href="https://smyl.es">Author Blog</a></li>
+				        <li>Version: ' . $version . '</li>
+				    </ul>';
+
+		return $sidebar;
+	}
 
 	public static function get_instance() {
 
 		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
+		if ( NULL == self::$instance ) {
 			self::$instance = new self;
 		}
 
 		return self::$instance;
 	}
 
-	function __construct(){
-		define("ADMINAREA", true);
-	}
-
-	public function output($vars){
-	    $sidebar = '<span class="header"><img src="images/icons/addonmodules.png" class="absmiddle" width="16" height="16" /> Example</span>
-	<ul class="menu">
-	        <li><a href="' . $modulelink . '">Demo Sidebar Content</a></li>
-	        <li>Version: ' . parent::$version . '</li>
-	    </ul>';
-	    parent::debug($sidebar, 'sidebar output');
-	    return $sidebar;
-	}
-
 }
-
-?>
